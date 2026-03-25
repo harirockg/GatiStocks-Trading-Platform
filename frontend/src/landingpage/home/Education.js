d64c01dbdbf875706dc97e0bf266548a0d846b59
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2026-03-25 16:01:44 +0530

    fix vercel build error

--- frontend/src/landingpage/home/Education.js
+++ frontend/src/landingpage/home/Education.js
@@ -12,14 +12,14 @@ function Education() {
             EduMastery, the largest online stock market education book in the world
             covering everything from the basics to advanced trading.
           </p>
-          <a href="" style={{ textDecoration: "none" }}>
+          <a href="#" style={{ textDecoration: "none" }}>
             EduMastery <i class="fa-solid fa-arrow-right-long"></i>
           </a>
           <p className="mt-5">
             TradingQ&A, the most active trading and investment community in
             India for all your market related queries.
           </p>
-          <a href="" style={{ textDecoration: "none" }}>
+          <a href="#" style={{ textDecoration: "none" }}>
             TradingQ&A <i class="fa-solid fa-arrow-right-long"></i>
           </a>
         </div>
