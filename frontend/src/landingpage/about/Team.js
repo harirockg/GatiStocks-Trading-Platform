4f8d387d7daef7e671b658029c9d1fbb2115f2f7
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2026-03-25 17:00:41 +0530

    updated image

--- frontend/src/landingpage/about/Team.js
+++ frontend/src/landingpage/about/Team.js
@@ -13,7 +13,7 @@ function Team() {
       >
         <div className="col-6 p-3 text-center">
           <img
-            src="media/images/harivansh.png"
+            src="media/images/Harivansh.png"
             style={{ borderRadius: "100%", width: "50%",  }}
           />
           <h4 className="mt-5">Harivansh Chauhan</h4>
--- frontend/src/landingpage/products/Universe.js
+++ frontend/src/landingpage/products/Universe.js
@@ -15,7 +15,7 @@ function Universe() {
           <p className="text-small text-muted">Thematic investment platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/streakk.png" />
+          <img src="media/images/Streakk.png" />
           <p className="text-small text-muted">Strategy management</p>
         </div>
         <div className="col-4 p-3 mt-5">
